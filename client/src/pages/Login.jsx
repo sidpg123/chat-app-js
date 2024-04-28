5a4f2051ec163c1542ef6160c2461c31fc460a07
sidpg123 <siddharthpatilgurav@gmail.com>
2024-04-28 20:22:33 +0530

    First commit

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,390 @@
+import { useFileHandler, useInputValidation } from "6pp";
+import { CameraAlt as CameraAltIcon } from "@mui/icons-material";
+import {
+  Avatar,
+  Button,
+  Container,
+  FormControl,
+  IconButton,
+  InputLabel,
+  MenuItem,
+  Paper,
+  Select,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import axios from "axios";
+import React, { useState } from "react";
+import toast from "react-hot-toast";
+import { useDispatch } from "react-redux";
+import { VisuallyHiddenInput } from "../components/styles/StyledComponents";
+import { bgGradient } from "../constants/color";
+import { server } from "../constants/config";
+import { userExists } from "../redux/reducers/auth";
+import { usernameValidator } from "../utils/validators";
+
+const Login = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [language, setLanguage] = useState("");
+
+  const languagehandler = (event) => {
+    setLanguage(event.target.value);
+  };
+
+  const toggleLogin = () => setIsLogin((prev) => !prev);
+
+  const name = useInputValidation("");
+  const bio = useInputValidation("");
+  const username = useInputValidation("", usernameValidator);
+  const password = useInputValidation("");
+  //const language = useInputValidation("")
+
+  const avatar = useFileHandler("single");
+
+  const dispatch = useDispatch();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    const toastId = toast.loading("Logging In...");
+
+    setIsLoading(true);
+    const config = {
+      withCredentials: true,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    try {
+      const { data } = await axios.post(
+        `${server}/api/v1/user/login`,
+        {
+          username: username.value,
+          password: password.value,
+        },
+        config
+      );
+      dispatch(userExists(data.user));
+      toast.success(data.message, {
+        id: toastId,
+      });
+    } catch (error) {
+      toast.error(error?.response?.data?.message || "Something Went Wrong", {
+        id: toastId,
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+
+    const toastId = toast.loading("Signing Up...");
+    setIsLoading(true);
+
+    const formData = new FormData();
+    formData.append("avatar", avatar.file);
+    formData.append("name", name.value);
+    formData.append("bio", bio.value);
+    formData.append("username", username.value);
+    formData.append("password", password.value);
+    formData.append("language", language);
+
+    const config = {
+      withCredentials: true,
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    };
+
+    try {
+      const { data } = await axios.post(
+        `${server}/api/v1/user/new`,
+        formData,
+        config
+      );
+
+      dispatch(userExists(data.user));
+      toast.success(data.message, {
+        id: toastId,
+      });
+    } catch (error) {
+      toast.error(error?.response?.data?.message || "Something Went Wrong", {
+        id: toastId,
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div
+      style={{
+        backgroundImage: bgGradient,
+      }}
+    >
+      <Container
+        component={"main"}
+        maxWidth="xs"
+        sx={{
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Paper
+          elevation={3}
+          sx={{
+            padding: 4,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          {isLogin ? (
+            <>
+              <Typography variant="h5">Login</Typography>
+              <form
+                style={{
+                  width: "100%",
+                  marginTop: "1rem",
+                }}
+                onSubmit={handleLogin}
+              >
+                <TextField
+                  required
+                  fullWidth
+                  label="Username"
+                  margin="normal"
+                  variant="outlined"
+                  value={username.value}
+                  onChange={username.changeHandler}
+                />
+
+                <TextField
+                  required
+                  fullWidth
+                  label="Password"
+                  type="password"
+                  margin="normal"
+                  variant="outlined"
+                  value={password.value}
+                  onChange={password.changeHandler}
+                />
+
+                <Button
+                  sx={{
+                    marginTop: "1rem",
+                  }}
+                  variant="contained"
+                  color="primary"
+                  type="submit"
+                  fullWidth
+                  disabled={isLoading}
+                >
+                  Login
+                </Button>
+
+                <Typography textAlign={"center"} m={"1rem"}>
+                  OR
+                </Typography>
+
+                <Button
+                  disabled={isLoading}
+                  fullWidth
+                  variant="text"
+                  onClick={toggleLogin}
+                >
+                  Sign Up Instead
+                </Button>
+              </form>
+            </>
+          ) : (
+            <>
+              <Typography variant="h5">Sign Up</Typography>
+              <form
+                style={{
+                  width: "100%",
+                  marginTop: "1rem",
+                }}
+                onSubmit={handleSignUp}
+              >
+                <Stack position={"relative"} width={"10rem"} margin={"auto"}>
+                  <Avatar
+                    sx={{
+                      width: "10rem",
+                      height: "10rem",
+                      objectFit: "contain",
+                    }}
+                    src={avatar.preview}
+                  />
+
+                  <IconButton
+                    sx={{
+                      position: "absolute",
+                      bottom: "0",
+                      right: "0",
+                      color: "white",
+                      bgcolor: "rgba(0,0,0,0.5)",
+                      ":hover": {
+                        bgcolor: "rgba(0,0,0,0.7)",
+                      },
+                    }}
+                    component="label"
+                  >
+                    <>
+                      <CameraAltIcon />
+                      <VisuallyHiddenInput
+                        type="file"
+                        onChange={avatar.changeHandler}
+                      />
+                    </>
+                  </IconButton>
+                </Stack>
+
+                {avatar.error && (
+                  <Typography
+                    m={"1rem auto"}
+                    width={"fit-content"}
+                    display={"block"}
+                    color="error"
+                    variant="caption"
+                  >
+                    {avatar.error}
+                  </Typography>
+                )}
+
+                <TextField
+                  required
+                  fullWidth
+                  label="Name"
+                  margin="normal"
+                  variant="outlined"
+                  value={name.value}
+                  onChange={name.changeHandler}
+                />
+                <FormControl sx={{ width: "100%" }}>
+                  <InputLabel id="language-label">Language</InputLabel>
+                  <Select label="Language" labelId="language-label" value={language} onChange={languagehandler}>
+                    {/* Indian Languages */}
+                    <MenuItem value={"hi"}>Hindi</MenuItem>
+                    <MenuItem value={"bn"}>Bengali</MenuItem>
+                    <MenuItem value={"te"}>Telugu</MenuItem>
+                    <MenuItem value={"mr"}>Marathi</MenuItem>
+                    <MenuItem value={"ta"}>Tamil</MenuItem>
+                    <MenuItem value={"ur"}>Urdu</MenuItem>
+                    <MenuItem value={"gu"}>Gujarati</MenuItem>
+                    <MenuItem value={"kn"}>Kannada</MenuItem>
+                    <MenuItem value={"ml"}>Malayalam</MenuItem>
+                    <MenuItem value={"pa"}>Punjabi</MenuItem>
+                    <MenuItem value={"or"}>Odia</MenuItem>
+                    <MenuItem value={"as"}>Assamese</MenuItem>
+                    <MenuItem value={"mr"}>Maithili</MenuItem>
+                    <MenuItem value={"sd"}>Sindhi</MenuItem>
+                    <MenuItem value={"ne"}>Nepali</MenuItem>
+                    <MenuItem value={"mr"}>Marwari</MenuItem>
+                    {/* Other Popular Languages */}
+                    <MenuItem value={"en"}>English</MenuItem>
+                    <MenuItem value={"es"}>Spanish</MenuItem>
+                    <MenuItem value={"zh"}>Chinese</MenuItem>
+                    <MenuItem value={"fr"}>French</MenuItem>
+                    <MenuItem value={"ar"}>Arabic</MenuItem>
+                    <MenuItem value={"pt"}>Portuguese</MenuItem>
+                    <MenuItem value={"ru"}>Russian</MenuItem>
+                    <MenuItem value={"de"}>German</MenuItem>
+                    <MenuItem value={"ja"}>Japanese</MenuItem>
+                    <MenuItem value={"ko"}>Korean</MenuItem>
+                    <MenuItem value={"it"}>Italian</MenuItem>
+                    <MenuItem value={"tr"}>Turkish</MenuItem>
+                    <MenuItem value={"vi"}>Vietnamese</MenuItem>
+                    <MenuItem value={"pl"}>Polish</MenuItem>
+                    <MenuItem value={"uk"}>Ukrainian</MenuItem>
+                    <MenuItem value={"nl"}>Dutch</MenuItem>
+                    <MenuItem value={"sv"}>Swedish</MenuItem>
+                    <MenuItem value={"th"}>Thai</MenuItem>
+                    <MenuItem value={"el"}>Greek</MenuItem>
+                    <MenuItem value={"cs"}>Czech</MenuItem>
+                    <MenuItem value={"ro"}>Romanian</MenuItem>
+                    <MenuItem value={"hu"}>Hungarian</MenuItem>
+                    <MenuItem value={"fi"}>Finnish</MenuItem>
+                    <MenuItem value={"da"}>Danish</MenuItem>
+                    {/* Add more languages as needed */}
+                  </Select>
+                </FormControl>
+
+                <TextField
+                  required
+                  fullWidth
+                  label="Bio"
+                  margin="normal"
+                  variant="outlined"
+                  value={bio.value}
+                  onChange={bio.changeHandler}
+                />
+                <TextField
+                  required
+                  fullWidth
+                  label="Username"
+                  margin="normal"
+                  variant="outlined"
+                  value={username.value}
+                  onChange={username.changeHandler}
+                />
+
+                {username.error && (
+                  <Typography color="error" variant="caption">
+                    {username.error}
+                  </Typography>
+                )}
+
+                <TextField
+                  required
+                  fullWidth
+                  label="Password"
+                  type="password"
+                  margin="normal"
+                  variant="outlined"
+                  value={password.value}
+                  onChange={password.changeHandler}
+                />
+
+                <Button
+                  sx={{
+                    marginTop: "1rem",
+                  }}
+                  variant="contained"
+                  color="primary"
+                  type="submit"
+                  fullWidth
+                  disabled={isLoading}
+                >
+                  Sign Up
+                </Button>
+
+                <Typography textAlign={"center"} m={"1rem"}>
+                  OR
+                </Typography>
+
+                <Button
+                  disabled={isLoading}
+                  fullWidth
+                  variant="text"
+                  onClick={toggleLogin}
+                >
+                  Login Instead
+                </Button>
+              </form>
+            </>
+          )}
+        </Paper>
+      </Container>
+    </div>
+  );
+};
+
+export default Login;
---
+++ server/app.js
@@ -0,0 +1,241 @@
+import express from "express";
+import { connectDB } from "./utils/features.js";
+import dotenv from "dotenv";
+import { errorMiddleware } from "./middlewares/error.js";
+import cookieParser from "cookie-parser";
+import { Server } from "socket.io";
+import { createServer } from "http";
+import { v4 as uuid } from "uuid";
+import cors from "cors";
+import axios from 'axios'; // Import axios library
+import { v2 as cloudinary } from "cloudinary";
+import {
+  CHAT_JOINED,
+  CHAT_LEAVED,
+  NEW_MESSAGE,
+  NEW_MESSAGE_ALERT,
+  ONLINE_USERS,
+  START_TYPING,
+  STOP_TYPING,
+} from "./constants/events.js";
+import { getSockets } from "./lib/helper.js";
+import { Message } from "./models/message.js";
+import { corsOptions } from "./constants/config.js";
+import { socketAuthenticator } from "./middlewares/auth.js";
+
+import userRoute from "./routes/user.js";
+import chatRoute from "./routes/chat.js";
+import adminRoute from "./routes/admin.js";
+import { log } from "console";
+
+dotenv.config({
+  path: "./.env",
+});
+
+const mongoURI = process.env.MONGO_URI;
+const port = process.env.PORT || 3000;
+const envMode = process.env.NODE_ENV.trim() || "PRODUCTION";
+const adminSecretKey = process.env.ADMIN_SECRET_KEY || "adsasdsdfsdfsdfd";
+const userSocketIDs = new Map();
+const onlineUsers = new Set();
+
+connectDB(mongoURI);
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  cors: corsOptions,
+});
+
+app.set("io", io);
+
+// Using Middlewares Here
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors(corsOptions));
+
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/chat", chatRoute);
+app.use("/api/v1/admin", adminRoute);
+
+
+// const axios = require('axios');
+
+async function detectLanguage(message) {
+  try {
+    const response = await axios.post('https://api.translateplus.io/v1/language_detect', {
+      text: message
+    }, {
+      headers: {
+        'X-API-KEY': '98e0df0cdd6d814cde9421d4cc8a8e24169fda75',
+        'Content-Type': 'application/json'
+      }
+    })
+    return response.data.language_detection.language;
+  } catch (error) {
+    console.error(error);
+    return message;
+  }
+}
+
+async function translateMessage(message, sourceLanguage, targetLanguage) {
+  try {
+    const response = await axios.post('https://api.translateplus.io/v1/translate', {
+      text: message,
+      source: sourceLanguage,
+      target: targetLanguage
+
+    }, {
+      headers: {
+        'X-API-KEY': '98e0df0cdd6d814cde9421d4cc8a8e24169fda75',
+        'Content-Type': 'application/json'
+      }
+    });
+    // console.count(response)
+    // console.log('response', response);
+    return response.data.translations.translation;
+  } catch (error) {
+    console.error('Error translating message:');
+    return message; // Return original message if translation fails
+  }
+}
+
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+io.use((socket, next) => {
+  cookieParser()(
+    socket.request,
+    socket.request.res,
+    async (err) => await socketAuthenticator(err, socket, next)
+  );
+});
+
+io.on("connection", (socket) => {
+
+
+  const user = socket.user;
+  //  console.log(user);
+  userSocketIDs.set(user._id.toString(), socket.id);
+
+  socket.on(NEW_MESSAGE, async ({ chatId, members, message }) => {
+    const sourceLanguage = user.language;
+    console.log("source", sourceLanguage);
+
+    // Translate the message if necessary
+    const translatedMessage = (sourceLanguage !== 'en')
+      ? await translateMessage(message, sourceLanguage, 'en')
+      : message;
+
+    // Get the sender's socket ID
+    console.log("Translated message: ", translateMessage);
+    const senderSocketId = userSocketIDs.get(user._id.toString());
+    console.log("sendersocket ", senderSocketId,);
+    // Emit original message to the sender's socket
+    await io.to(senderSocketId).emit(NEW_MESSAGE, {
+      chatId,
+      message: {
+        content: message,
+        _id: uuid(),
+        sender: {
+          _id: user._id,
+          name: user.name,
+        },
+        chat: chatId,
+        createdAt: new Date().toISOString(),
+      }
+    });
+
+    // Iterate through each member in the conversation
+    await Promise.all(members.map(async (member) => {
+      // Translate the message if necessary
+      console.log('member', member);
+      const receiverSocketId = member ? userSocketIDs.get(member.toString()) : null;
+
+      if (receiverSocketId) {
+        console.log('Sending translated message to receiver:', translatedMessage);
+
+        // Emit translated message to the receiver's socket
+        await io.to(receiverSocketId).emit(NEW_MESSAGE, {
+          chatId,
+          message: {
+            content: translatedMessage,
+            _id: uuid(),
+            sender: {
+              _id: user._id,
+              name: user.name,
+            },
+            chat: chatId,
+            createdAt: new Date().toISOString(),
+          }
+        });
+      } else {
+        console.error('Receiver socket ID not available for member:', member);
+      }
+    }));
+
+    // Emit a new message alert to all members except the sender
+    const receiverIds = members.filter(member => member !== user._id);
+    io.to(getSockets(receiverIds)).emit(NEW_MESSAGE_ALERT, { chatId });
+
+    try {
+      // Save the original message to the database
+      await Message.create({
+        content: translatedMessage,
+        sender: user._id,
+        chat: chatId,
+      });
+    } catch (error) {
+      console.error('Error saving message to database:', error);
+    }
+  });
+
+
+
+
+  socket.on(START_TYPING, ({ members, chatId }) => {
+    const membersSockets = getSockets(members);
+    socket.to(membersSockets).emit(START_TYPING, { chatId });
+  });
+
+  socket.on(STOP_TYPING, ({ members, chatId }) => {
+    const membersSockets = getSockets(members);
+    socket.to(membersSockets).emit(STOP_TYPING, { chatId });
+  });
+
+  socket.on(CHAT_JOINED, ({ userId, members }) => {
+    onlineUsers.add(userId.toString());
+
+    const membersSocket = getSockets(members);
+    io.to(membersSocket).emit(ONLINE_USERS, Array.from(onlineUsers));
+  });
+
+  socket.on(CHAT_LEAVED, ({ userId, members }) => {
+    onlineUsers.delete(userId.toString());
+
+    const membersSocket = getSockets(members);
+    io.to(membersSocket).emit(ONLINE_USERS, Array.from(onlineUsers));
+  });
+
+  socket.on("disconnect", () => {
+    userSocketIDs.delete(user._id.toString());
+    onlineUsers.delete(user._id.toString());
+    socket.broadcast.emit(ONLINE_USERS, Array.from(onlineUsers));
+  });
+});
+
+app.use(errorMiddleware);
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port} in ${envMode} Mode`);
+});
+
+export { envMode, adminSecretKey, userSocketIDs };
